f34cb2517579797312c7035394e6908dfda4366b
Nikolai Rubashov <nikolai.salmanovich@gmail.com>
2012-06-21 10:15:02 +0900

    Quickfix for Lua 5.2.  Thanks to Sergey Rozhenko for the setfenv emulation.

--- src/luaspec.lua
+++ src/luaspec.lua
@@ -171,6 +171,21 @@ end
 
 --
 
+-- From http://lua-users.org/lists/lua-l/2010-06/msg00313.html
+setfenv = setfenv or function(f, t)
+	f = (type(f) == 'function' and f or debug.getinfo(f + 1, 'f').func)
+	local name
+	local up = 0
+	repeat
+	    up = up + 1
+	    name = debug.getupvalue(f, up)
+	until name == '_ENV' or name == nil
+	if name then
+		debug.upvaluejoin(f, up, function() return name end, 1) -- use unique upvalue
+			debug.setupvalue(f, up, t)
+	end
+end
+
 Context = {}
 Context.__index = Context
 
